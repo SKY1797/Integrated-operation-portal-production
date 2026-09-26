f4e02f7993de7570cdb5a3f91291f6cc9bba0098
AKASH KANTARIA <68785054+SKY1797@users.noreply.github.com>
2026-09-26 16:46:52 +0530

    Update app.js

--- app.js
+++ app.js
@@ -897,7 +897,7 @@ function renderObservation() {
                 
                 <div>
                     <label class="list-label" style="display:block; margin-bottom:0.4rem;">Date of Visit *</label>
-                    <input type="text" onfocus="(this.type='date')" onblur="if(!this.value) this.type='text'" id="obsDate" class="ui-input" value="${today}" required style="width: 100%; box-sizing: border-box; display: block; padding: 0.75rem;">
+                    <input type="date" id="obsDate" class="ui-input" value="${today}" required style="width: 100% !important; max-width: 100% !important; box-sizing: border-box !important; margin: 0 !important; -webkit-appearance: none; appearance: none;">
                 </div>
                 
                 <div>
